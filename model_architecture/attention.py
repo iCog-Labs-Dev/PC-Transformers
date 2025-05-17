5f5e4571a6cce42d1c1918e0b662ebc6aa657ef3
Berekto <bereketalebachew87@gmail.com>
2025-05-17 11:21:18 +0300

    Remove flash

--- model_architecture/attention.py
+++ model_architecture/attention.py
@@ -26,16 +26,11 @@ class CausalSelfAttention(nn.Module):
          self.attn_dropout = nn.Dropout(config.dropout)
          self.resid_dropout = nn.Dropout(config.dropout)
 
-         # Flash attention
-         self.flash = hasattr(F, 'scaled_dot_product_attention')
 
-         if not self.flash:
-            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-            # Causal mask for auto-regressive attention
-            self.register_buffer(
-                "causal_mask",
-                torch.tril(torch.ones(config.block_size, config.block_size))
-                     .view(1, 1, config.block_size, config.block_size)
+         self.register_buffer(
+            "causal_mask",
+            torch.tril(torch.ones(config.block_size, config.block_size))
+                    .view(1, 1, config.block_size, config.block_size)
             )
 
 
@@ -53,18 +48,11 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
         
         
-        if self.flash:
-            attn_out = F.scaled_dot_product_attention(
-                q, k, v, 
-                attn_mask=None, 
-                dropout_p=self.dropout if self.training else 0.0,
-                is_causal=True)
-        else:
-            attn_score = (q @ k.transpose(-2, -1)) / math.sqrt(self.head_dim)
-            attn_score = attn_score.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('inf'))
-            attn_weights = F.softmax(attn_score, dim=-1)
-            attn_weights = self.attn_dropout(attn_weights)
-            attn_out = attn_weights @ v
+        attn_score = (q @ k.transpose(-2, -1)) / math.sqrt(self.head_dim)
+        attn_score = attn_score.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('inf'))
+        attn_weights = F.softmax(attn_score, dim=-1)
+        attn_weights = self.attn_dropout(attn_weights)
+        attn_out = attn_weights @ v
 
         # Merge heads back: [B, T, C]
         attn_out = attn_out.transpose(1, 2).contiguous().view(B, T, C)
