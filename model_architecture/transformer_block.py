e16d2c08a4f976621ca40ce2946794094051e1d0
quinbez <bezawitt9@gmail.com>
2025-05-22 11:06:18 +0300

    feat: modify Transformer block forward pass to better suit training

--- model_architecture/transformer_block.py
+++ model_architecture/transformer_block.py
@@ -1,16 +1,20 @@
 import torch.nn as nn
-from .attention import CausalSelfAttention
+from .attention import Attention
 from .mlp import MLP
 
 class TransformerBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln1 = nn.LayerNorm(config.n_embd)
-        self.attn = CausalSelfAttention(config)
+        self.attn = Attention(config)
         self.ln2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
 
-    def forward(self, x):
-        x = x + self.attn(self.ln1(x))
-        x = x + self.mlp(self.ln2(x))
-        return x
+    def forward(self, output_x):
+        output_x = self.ln2(output_x)
+        fc1_x = self.mlp(output_x)
+
+        fc1_x = self.ln1(fc1_x)
+        x_qkv = self.attn(fc1_x)
+
+        return x_qkv
