05439ef266cae0912df21205f757526ac8ae076d
Berekto <bereketalebachew87@gmail.com>
2025-05-28 16:24:36 +0300

    refactor(attention): resturcute for parallel execution

--- model_architecture/attention.py
+++ model_architecture/attention.py
@@ -13,9 +13,9 @@ class Attention(nn.Module):
          self.head_dim = config.n_embed // config.num_heads
          self.dropout = nn.Dropout(config.dropout)
 
-         self.q = nn.Linear(config.n_embed, config.n_embed)
-         self.k = nn.Linear(config.n_embed, config.n_embed)
-         self.v = nn.Linear(config.n_embed, config.n_embed)
+         self.query= nn.Linear(config.n_embed, config.n_embed)
+         self.key = nn.Linear(config.n_embed, config.n_embed)
+         self.value = nn.Linear(config.n_embed, config.n_embed)
          self.output = nn.Linear(config.n_embed, config.n_embed)
         
          self.pc_qkv=PCLayer(T= config.T,
@@ -31,26 +31,13 @@ class Attention(nn.Module):
             update_bias = config.update_bias
         )
 
-    def forward(self, fc1_x):
-        self.pc_output(target_activity = fc1_x, layer = self.output, layer_type = "output_attn")
-        att_score = self.pc_output.get_x("output_attn")
-
-        self.pc_qkv(target_activity = att_score, proj_layers={
-            "q_proj": self.q,
-            "k_proj": self.k,
-            "v_proj": self.v
-        }, layer_type = "attn")
-
-        x_qkv = self.pc_qkv.get_x("attn")
-
-        return x_qkv
     
     def evaluate(self, x):
         batch_size, seq_len, _ = x.size()
         
-        Q = self.q(x)
-        K = self.k(x)
-        V = self.v(x)
+        Q = self.query(x)
+        K = self.key(x)
+        V = self.value(x)
 
         # Reshape for multi-head: [B, T, H, D/H] → [B, H, T, D/H]
         Q = Q.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
