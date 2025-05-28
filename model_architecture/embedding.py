d7c28a94eaf6942a66e831d7932bb551aa3c1110
Berekto <bereketalebachew87@gmail.com>
2025-05-28 16:25:25 +0300

    refactor(embedding): adapt embedding for parallelism

--- model_architecture/embedding.py
+++ model_architecture/embedding.py
@@ -16,22 +16,14 @@ class Embedding_Layer(nn.Module):
                                is_holding_error= config.is_holding_error,
                                update_bias = config.update_bias,
                                )
-      
-    def forward(self, x_qkv, input_ids, position_ids)-> torch.Tensor:
-        if position_ids is None:
-           position_ids = torch.arange(0, input_ids.size(1), dtype=torch.long)
-           position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
 
-        embeddings = self.pc_layer(x_qkv, layer={
-            "word": self.word_embeddings,
-            "pos": self.position_embeddings
-        }, layer_type="embed", input_ids = input_ids, position_ids = position_ids)
-        
-        return embeddings
     
-    def evaluate(self, input_ids):
-        word_embed=self.word_embeddings(input_ids)
-        position_ids = torch.arange(word_embed.size(1), device=input_ids.device).unsqueeze(0)
+    def evaluate(self, input_ids, position_ids=None):
+        word_embed = self.word_embeddings(input_ids)
+        if position_ids is None:
+            position_ids = torch.arange(
+                word_embed.size(1), device=input_ids.device
+            ).unsqueeze(0).expand_as(input_ids)
         pos_embed=self.position_embeddings(position_ids)
         embeddings = word_embed + pos_embed
         embeddings = self.LayerNorm(embeddings)
