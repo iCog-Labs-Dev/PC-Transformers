cad55aa9f6dc7d667ee89eb401129ec591e2673b
Nardos Tatek <narditatek@gmail.com>
2025-06-14 20:52:27 +0200

    Add lambda regularizer to diversity gradient (dvl_grad)

--- predictive_coding/config.py
+++ predictive_coding/config.py
@@ -4,13 +4,14 @@ from dataclasses import dataclass
 class GPTConfig:
     vocab_size: int
     block_size: int
-    n_embed: int =64
+    la: float=0.5
+    n_embed: int =516
     dropout: float = 0.1
-    local_learning_rate: float = 1e-3
+    local_learning_rate: float = 1e-5
     T: int = 10
     is_holding_error: bool = False
     update_bias: bool = True
-    num_heads: int = 2
+    num_heads: int = 12
     n_blocks: int = 4
     batch_size: int = 8
     num_epochs: int = 5
