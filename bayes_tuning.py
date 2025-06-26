4db0651b0756a0a57e7ae04b08e0e34ae8b1e476
quinbez <bezawitt9@gmail.com>
2025-06-26 23:26:48 +0300

    update bayes_tuning to unpack all three values

--- bayes_tuning.py
+++ bayes_tuning.py
@@ -219,7 +219,7 @@ def objective(trial):
         try:
             model.eval()
             max_val_batches = min(10, len(valid_loader))
-            avg_energy, val_loss = evaluate(model, valid_loader, tokenizer, max_batches=max_val_batches)
+            avg_energy, val_loss, avg_perplexity = evaluate(model, valid_loader, tokenizer, max_batches=max_val_batches)
             normalized_energy = normalize_energy(avg_energy, config.energy_fn_name)
 
             trial_time = time.time() - start_time
