56bd9adb22adeb87d3f64bb964de7a951b845f78
Yonatan Getachew <yonatangetachew91@gmail.com>
2025-06-28 17:01:07 +0300

    added the flash attention to the config object

--- predictive_coding/config.py
+++ predictive_coding/config.py
@@ -26,6 +26,7 @@ class GPTConfig:
         num_epochs (int): Number of training epochs.
         use_lateral (bool): Whether to use lateral (recurrent) connections.
         energy_fn_name (str): Name of the energy function to use for error computation.
+        use_flash_attention (bool): Whether to use FlashAttention.
     """
     vocab_size: int
     block_size: int
