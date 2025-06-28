ddac08b8b22028812d1b9f7d84c9873322857c51
quinbez <bezawitt9@gmail.com>
2025-06-28 15:30:12 +0300

    refactor: wrap datasets, and dataloaders code in function for reuse and clarity.

--- Data_preprocessing/dataloader.py
+++ Data_preprocessing/dataloader.py
@@ -3,28 +3,23 @@ from .datasets.penn_treebank import PennTreebankDataset
 from .config import Config
 from utils.model_utils import pad_collate_fn, load_tokenizer
 
-train_dataset = PennTreebankDataset("train_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-valid_dataset = PennTreebankDataset("valid_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-test_dataset = PennTreebankDataset("test_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-
 tokenizer = load_tokenizer()
 pad_token_id = tokenizer.token_to_id("[PAD]")
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=64,
-    shuffle=True,
-    collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id)
-)
 
-valid_loader = DataLoader(
-    valid_dataset,
-    batch_size=64,
-    collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id)
-)
+def get_datasets():
+    train_dataset = PennTreebankDataset("train_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
+    valid_dataset = PennTreebankDataset("valid_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
+    test_dataset = PennTreebankDataset("test_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
 
-test_loader = DataLoader(
-    test_dataset,
-    batch_size=64,
-    collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id)
-)
+    return train_dataset, valid_dataset, test_dataset
 
+def get_loaders():
+    train_dataset, valid_dataset, test_dataset = get_datasets()
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, 
+                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+    valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle = True,
+                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle = True,
+                             collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+
+    return train_loader, valid_loader, test_loader
