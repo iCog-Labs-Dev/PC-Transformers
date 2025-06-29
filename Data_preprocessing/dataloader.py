f0a0d037a9380243fcf3b1d5eacfcd7a5becce82
quinbez <bezawitt9@gmail.com>
2025-06-29 13:00:57 +0300

    refactor: wrap datasets, and dataloaders code in function for reuse and clarity.

--- Data_preprocessing/dataloader.py
+++ Data_preprocessing/dataloader.py
@@ -1,31 +1,26 @@
 
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from Data_preprocessing.datasets.merged_sets import TokenizedDataset
 from Data_preprocessing.config import Config
 from utils.model_utils import pad_collate_fn, load_tokenizer
 
-train_dataset = TokenizedDataset("train", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-valid_dataset = TokenizedDataset("valid", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-test_dataset = TokenizedDataset("test", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-
 tokenizer = load_tokenizer()
 pad_token_id = tokenizer.pad_token_id
 
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=Config.BATCH_SIZE,
-    shuffle=True,
-    collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id)
-)
+def get_datasets():
+    train_dataset = TokenizedDataset("train", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
+    valid_dataset = TokenizedDataset("valid", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
+    test_dataset = TokenizedDataset("test", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
 
-valid_loader = DataLoader(
-    valid_dataset,
-    batch_size=Config.BATCH_SIZE,
-    collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id)
-)
+    return train_dataset, valid_dataset, test_dataset
 
-test_loader = DataLoader(
-    test_dataset,
-    batch_size=Config.BATCH_SIZE,
-    collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id)
-) 
+def get_loaders():
+    train_dataset, valid_dataset, test_dataset = get_datasets()
+    train_loader = DataLoader(train_dataset, batch_size=Config.BATCH_SIZE, shuffle=True, 
+                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+    valid_loader = DataLoader(valid_dataset, batch_size=Config.BATCH_SIZE, shuffle = True,
+                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+    test_loader = DataLoader(test_dataset, batch_size=Config.BATCH_SIZE, shuffle = True,
+                             collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+
+    return train_loader, valid_loader, test_loader
