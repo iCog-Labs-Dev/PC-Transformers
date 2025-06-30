8bb50e2f7f0ad24934b8b9bb7eef0293e834193d
quinbez <bezawitt9@gmail.com>
2025-07-01 01:23:44 +0300

    refactor: Update configuration and data loading in Data_preprocessing

--- Data_preprocessing/dataloader.py
+++ Data_preprocessing/dataloader.py
@@ -7,19 +7,27 @@ tokenizer = load_tokenizer()
 pad_token_id = tokenizer.token_to_id("[PAD]")
 
 def get_datasets():
-    train_dataset = PennTreebankDataset("train_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-    valid_dataset = PennTreebankDataset("valid_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
-    test_dataset = PennTreebankDataset("test_ids.pkl", Config.TOKENIZER_DIR, Config.MAX_LENGTH)
+    train_dataset = PennTreebankDataset("train_ids.pkl", Config.tokenizer_dir, Config.max_length)
+    valid_dataset = PennTreebankDataset("valid_ids.pkl", Config.tokenizer_dir, Config.max_length)
+    test_dataset = PennTreebankDataset("test_ids.pkl", Config.tokenizer_dir, Config.max_length)
 
     return train_dataset, valid_dataset, test_dataset
 
 def get_loaders():
     train_dataset, valid_dataset, test_dataset = get_datasets()
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, 
-                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
-    valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle = True,
-                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle = True,
-                             collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id))
+    train_loader = DataLoader(train_dataset, batch_size = Config.batch_size, shuffle=True, 
+                              num_workers = Config.num_workers, pin_memory = True,
+                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id),
+                              persistent_workers = Config.num_workers > 0)
+
+    valid_loader = DataLoader(valid_dataset, batch_size=Config.batch_size, shuffle = False,
+                              num_workers=Config.num_workers, pin_memory = True,
+                              collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id),
+                              persistent_workers=Config.num_workers > 0)
+                              
+    test_loader = DataLoader(test_dataset, batch_size=Config.batch_size, shuffle = False,
+                             num_workers=Config.num_workers, pin_memory = True,
+                             collate_fn=lambda batch: pad_collate_fn(batch, pad_token_id),
+                             persistent_workers=Config.num_workers > 0)
 
     return train_loader, valid_loader, test_loader
