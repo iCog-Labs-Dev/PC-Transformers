c5aef69e34ef36bf56f34362d12e9618f4aba4ec
quinbez <bezawitt9@gmail.com>
2025-07-09 12:27:15 +0300

    minor change

--- Data_preprocessing/config.py
+++ Data_preprocessing/config.py
@@ -4,7 +4,7 @@ class Config:
     PAD_ID=None
     EOS_ID=None
     MAX_LENGTH = 128
-    DATASET_NAME = "ptb"  # "ptb" for penntreebank "opwb" for OpenWebText
+    DATASET_NAME = "opwb"  # "ptb" for penntreebank "opwb" for OpenWebText
     BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
     DATA_DIR = os.path.join(BASE_DIR, "Data") 
     TOKENIZER_DIR = os.path.join(BASE_DIR, "tokenizer", "outputs")  
