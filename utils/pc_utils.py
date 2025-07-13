e790726496b1701359c08095362769bb1e575202
quinbez <bezawitt9@gmail.com>
2025-07-13 08:47:18 +0000

    fix: avoid calling GPTConfig class and use the default values of num_heads, embeddings, and num_embeds.

--- utils/pc_utils.py
+++ utils/pc_utils.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-from predictive_coding.config import GPTConfig
 from typing import Tuple
 from utils.attention_utils import apply_flash_attention, apply_standard_attention
 
@@ -192,7 +191,7 @@ def step_linear(t, T, target, x, layer, W_latents, layer_type, local_lr, clamp_v
 
     return x, mu
 
-def step_attn(t, T, target, x, W_latents, proj_layers, layer_type, local_lr, clamp_value, use_lateral, is_holding_error, energy_fn_name, update_bias, requires_update, layer_instance, flash=False):
+def step_attn(t, T, target, x, W_latents, proj_layers, layer_type, local_lr, clamp_value, use_lateral, is_holding_error, energy_fn_name, update_bias, requires_update, layer_instance, num_heads, n_embed, la, flash=False):
         assert proj_layers is not None, "proj_layers dict is required for attention"
         device = x.device
         q_proj = proj_layers.get("q_proj", None)
@@ -204,9 +203,8 @@ def step_attn(t, T, target, x, W_latents, proj_layers, layer_type, local_lr, cla
         V= v_proj(x)
         batch_size, seq_len, embed_dim=target.shape
         
-        num_heads = GPTConfig.num_heads
-        head_dim = GPTConfig.n_embed // GPTConfig.num_heads 
-        la= GPTConfig.la * math.sqrt(1.0 / head_dim)
+        head_dim = n_embed // num_heads 
+        la= la * math.sqrt(1.0 / head_dim)
 
         Q = Q.view(batch_size, num_heads, seq_len, head_dim).transpose(1, 2) # B. H, T, D
         K = K.view(batch_size, num_heads, seq_len, head_dim).transpose(1, 2)
