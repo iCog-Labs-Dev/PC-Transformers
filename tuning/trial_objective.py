98932924827f40cb76c75c35635c7854ba7ccb92
Bethlehem <93065948+Betty987@users.noreply.github.com>
2025-08-20 15:06:00 +0300

    fix:use the same local_Rank setup as model_arch branch

--- tuning/trial_objective.py
+++ tuning/trial_objective.py
@@ -14,6 +14,7 @@ from tuning.dataloader import get_dynamic_batch_size, create_subset_loaders
 from tuning.tuning_logs import log_trial_to_detailed_log
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
+from utils.device_utils import setup_device
 
 def broadcast_config(config_dict, device):
     """Broadcast config from rank 0 to all other ranks"""
@@ -36,41 +37,59 @@ def objective(trial, device = None, flash=False):
     print(f"\nStarting Trial {trial.number}")
     
     try:
-        if "RANK" in os.environ and torch.cuda.is_available():
-            if not dist.is_initialized():
-                dist.init_process_group(backend="gloo")
-            local_rank = int(os.environ["LOCAL_RANK"])
-            device = torch.device(f"cuda:{local_rank}")
-            torch.cuda.set_device(local_rank)
-        else:
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            
+        # if "RANK" in os.environ and torch.cuda.is_available():
+        #     if not dist.is_initialized():
+        #         dist.init_process_group(backend="gloo")
+        #     local_rank = int(os.environ["LOCAL_RANK"])
+        #     device = torch.device(f"cuda:{local_rank}")
+        #     torch.cuda.set_device(local_rank)
+        # else:
+        #     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        local_rank, device, _ = setup_device()
         tokenizer = load_tokenizer()
         vocab_size = len(tokenizer)
 
-        if dist.is_initialized():
-            if dist.get_rank() == 0:
-                config = get_dynamic_model_config(trial, vocab_size, flash=flash)
-                if config is None:
-                    return float("inf")
-                config_dict = config.__dict__
-            else:
-                config_dict = None
-
-            config_dict = broadcast_config(config_dict, device)
-            config = GPTConfig(**config_dict)
-            update_global_config(config.__dict__)
+        # if dist.is_initialized():
+        #     if dist.get_rank() == 0:
+        #         config = get_dynamic_model_config(trial, vocab_size, flash=flash)
+        #         if config is None:
+        #             return float("inf")
+        #         config_dict = config.__dict__
+        #     else:
+        #         config_dict = None
         
-        else:
-            config = get_dynamic_model_config(trial, vocab_size, flash=flash)
+
+        #     config_dict = broadcast_config(config_dict, device)
+        #     config = GPTConfig(**config_dict)
+        #     update_global_config(config.__dict__)
+        
+        # else:
+        #     config = get_dynamic_model_config(trial, vocab_size, flash=flash)
+        #     if config is None:
+        #         return float("inf")
+        #     update_global_config(config.__dict__)
+        if not dist.is_initialized() or dist.get_rank() == 0:
+            config = get_dynamic_model_config(trial, vocab_size, flash)
             if config is None:
                 return float("inf")
-            update_global_config(config.__dict__)
+            config_dict = config.__dict__
+        else:
+            config_dict = None
+
+        if dist.is_initialized():
+            config_dict = broadcast_config(config_dict, device)
+        
+        config = GPTConfig(**config_dict)
+        update_global_config(config.__dict__)
 
         model = PCTransformer(config).to(device)  
+        # if dist.is_initialized():
+        #     model = DDP(model, device_ids=[device.index], output_device=device.index)
         if dist.is_initialized():
-            model = DDP(model, device_ids=[device.index], output_device=device.index)
-
+            if device.type == "cuda":
+                model = DDP(model, device_ids=[device.index], output_device=device.index)
+            else:
+                model = DDP(model)
         batch_size = get_dynamic_batch_size(config.n_embed, config.block_size)
         train_loader, valid_loader = create_subset_loaders(batch_size=batch_size, distributed=dist.is_initialized())
 
